64f04a14d26203cf5e98c4869b9c819e8db22b60
Ryan queva <quevaqueva11@gmail.com>
2022-06-02 19:00:31 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# AQCM05
+ Page web de la sonde AQCM05 sur le site du lycée
---
+++ site web/js/main.js
@@ -0,0 +1,31 @@
+function afficherMasquer(id)
+{
+if(document.getElementById("text1").style.display == "none")
+    document.getElementById("text1").style.display = "block";
+else
+    document.getElementById("text1").style.display = "none";
+}
+
+function afficherMasquer2(id)
+{
+if(document.getElementById("text2").style.display == "none")
+    document.getElementById("text2").style.display = "block";
+else
+    document.getElementById("text2").style.display = "none";
+}
+
+function afficherMasquer3(id)
+{
+if(document.getElementById("text3").style.display == "none")
+    document.getElementById("text3").style.display = "block";
+else
+    document.getElementById("text3").style.display = "none";
+}
+
+function afficherMasquerphoto(id)
+{
+if(document.getElementById("photosondesaqcm05").style.display == "none")
+    document.getElementById("photosondesaqcm05").style.display = "block";
+else
+    document.getElementById("photosondesaqcm05").style.display = "none";
+}
